c4f5e37926fb2867cff8c25c491ea422637ce3aa
Rose Galindo <158229168+Rosegalindo@users.noreply.github.com>
2024-03-12 20:18:42 -0300

    New ex008.js

---
+++ Aula11/ex008.js
@@ -0,0 +1,4 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+console.log(`Dirija sempre usando cinto de segurança!`)
