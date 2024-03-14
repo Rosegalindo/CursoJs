6c3d44c5ca0e7d9f3ae7cc0d7a12c21f28996ab0
Rose Galindo <158229168+Rosegalindo@users.noreply.github.com>
2024-03-14 16:49:21 -0300

    mudei as imagnes

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // Bom dia!
+        img.src = 'manha3.png'
+        document.body.style.background = '#e2c9b1'
+    } else if (hora>= 12 && hora < 18) {
+        //Boa tarde!
+        img.src = 'tarde2.png'
+        document.body.style.background = '#839cb2'
+    } else {
+        //Boa noite!
+        img.src = 'noite1.png'
+        document.body.style.background = '#173642'
+    }
+}
